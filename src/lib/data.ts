c3c723cc1500ea4a5c5ca4365de070be13d789af
Sathya1245632 <sksathya009@gmail.com>
2026-03-16 00:50:29 +0530

    add alleppey image

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -335,7 +335,7 @@ export const destinations = [
         id: 'alleppey',
         name: 'Alleppey',
         state: 'South India',
-        image: '/images/kanyakumari.jpg',
+        image: '/images/alleppey.png',
         description: 'Explore the divine beauty and rich heritage of Alleppey.',
         rating: 4.8,
         reviews: Math.floor(Math.random() * 5000) + 1000,
